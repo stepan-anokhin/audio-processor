fda3297cc9e36f12ac4d014741bc1460339b180d
Stepan Anokhin <stepan.anokhin@gmail.com>
2024-08-27 13:34:20 +0700

    Refactor speed perturbance

--- audio_transformers/core/speed_perturbation.py
+++ audio_transformers/core/speed_perturbation.py
@@ -1,6 +1,7 @@
 import math
 
 import numpy as np
+from scipy.interpolate import interp1d
 from scipy.signal import ShortTimeFFT
 from scipy.signal.windows import gaussian
 
@@ -20,23 +21,32 @@ class SpeedPerturbation(Transform):
         self.window_size: float = window_size
 
     def __call__(self, signal: Signal) -> Signal:
+        # Calculate spectrogram or original signal
         window_size_samples = int(self.window_size * signal.rate)  # Window size in samples
         hop_samples = window_size_samples // 2
         window = gaussian(window_size_samples, std=window_size_samples // 2, sym=True)
         stft = ShortTimeFFT(window, hop=hop_samples, fs=signal.rate, scale_to="magnitude")
         spectre = stft.stft(signal.data)
 
-        # Now we need to stretch or squeeze spectre depending on the speed factor
-        spectre_samples = spectre.shape[-1]
-        target_samples = math.floor(spectre_samples / self.speed_factor)  # Number of samples in target spectre
-        target_shape = list(spectre.shape)
-        target_shape[-1] = target_samples
-        target_spectre = np.zeros(target_shape, dtype=spectre.dtype)
-        per_sample = spectre_samples / target_samples
-        for i in range(target_samples):
-            start_index = math.floor(per_sample * i)
-            end_index = math.floor(per_sample * (i + 1)) + 1
-            target_spectre[:, :, i] = spectre[:, :, start_index:end_index].mean(axis=-1)
+        # Now we need to stretch or squeeze spectrogram depending on the speed factor
 
+        # Initialize empty result array
+        orig_spectre_samples = spectre.shape[-1]
+        target_spectre_samples = max(math.floor(orig_spectre_samples / self.speed_factor), 1)
+        target_shape = list(spectre.shape)
+        target_shape[-1] = target_spectre_samples
+        target_spectre = np.zeros(target_shape, dtype=spectre.dtype)
+
+        # Represent spectre as a function of time (measured in
+        # sample index) by linear interpolation along the time axis.
+        time_values = range(orig_spectre_samples)
+        spectre_func = interp1d(time_values, spectre, axis=-1, bounds_error=False, fill_value=0.0)
+
+        # Fill the target spectre
+        for time in range(target_spectre_samples):
+            orig_time = orig_spectre_samples / target_spectre_samples * time
+            target_spectre[:, :, time] = spectre_func(orig_time)
+
+        # Reverse Short-Time Fourier Transform
         output = stft.istft(target_spectre)
         return Signal(output, signal.rate)
